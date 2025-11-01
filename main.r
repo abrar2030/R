ffaa8aaedbe0f43797c3b0e6b451549d76bec53a
abrar2030 <abrarahmedpei@gmail.com>
2025-11-01 07:02:31 +0500

    Refactor code and enhance maintainability.

---
+++ main.r
@@ -0,0 +1,12 @@
+is_prime <- function(n) {
+  if (n <= 1) return(FALSE)
+  for (i in 2:sqrt(n)) {
+    if (n %% i == 0) return(FALSE)
+  }
+  return(TRUE)
+}
+
+
+print(is_prime(7))   # TRUE
+print(is_prime(10))  # FALSE
+
